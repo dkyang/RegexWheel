8fddad76f5643b21d4568c102df4d3b212590df0
dkyang <yangzhuangsuan@gmail.com>
2013-12-06 15:23:27 +0800

    add base class of nfa and dfa, and some nested classes are moved to base class

---
+++ fa.cc
@@ -0,0 +1,41 @@
+#include "fa.h"
+
+void FA::State::AddOutEdge(State *to, char alpha)
+{
+	Edge *edge = new Edge(this, to, alpha);
+	out_edges_.push_back(edge);
+}
+
+// epsilon-edge
+void FA::State::AddOutEdge(State *to) 
+{
+	Edge *edge = new Edge(this, to);
+	out_edges_.push_back(edge);
+}
+
+FA::State::~State() 
+{
+	// Edge的内存分配全都在State内部，所以可以在析构函数中释放，
+	// 保证没有内存泄露
+	for (std::vector<Edge*>::iterator vit = out_edges_.begin(); 
+		vit != out_edges_.end(); vit++) {
+			delete *vit;
+			*vit = NULL;
+	}
+}
+
+FA::State* FA::AllocState() 
+{
+	State *state = new State(state_count_++);
+	state_list_.push_back(state); // 记录，以便释放内存
+	return state;
+}
+
+// 释放动态分配的State内存
+void FA::FreeStates()
+{
+	for (std::list<State*>::iterator lit = state_list_.begin(); 
+		lit != state_list_.end(); lit++) {
+			delete *lit;
+	}
+}
---
+++ fa.h
@@ -0,0 +1,90 @@
+#ifndef _FA_H_
+#define _FA_H_
+
+#include <list>
+#include <vector>
+
+// NFA和DFA的基类
+class FA 
+{
+public:
+	FA() : state_count_(0), start_state_(NULL), end_state_(NULL)
+	{	}
+
+protected:
+
+	// nested class of NFA
+	// 前向声明
+	class State;
+	class Edge;
+	class StateSet;
+
+	class State 
+	{
+	public:
+		State(int state) 
+			: state_(state)
+		{	}
+
+		void AddOutEdge(State *to, char alpha);
+
+		// epsilon-edge
+		void AddOutEdge(State *to);
+
+		~State();
+
+		int state_;
+		//type: begin, terminal, normal
+		std::vector<Edge*> out_edges_;
+	};
+
+	class Edge
+	{
+	public:
+		Edge(State *from, State *to, char alpha) 
+			: from_(from), to_(to), is_epsilon_(false), alpha_(alpha)
+		{	}
+
+		// epsilon edge
+		Edge(State *from, State *to) 
+			: from_(from), to_(to), is_epsilon_(true)
+		{	}
+
+		bool is_epsilon()
+		{
+			return is_epsilon_;
+		}
+
+		State *from_;
+		State *to_;
+		bool is_epsilon_;
+		char alpha_;
+	};
+
+	// 辅助数据结构，用于在re->NFA的转换过程中保存中间结果
+	class StateSet {
+	public:
+		StateSet(State *start, State *end) 
+			: start_(start), end_(end) 
+		{	}
+		State *start_;
+		State *end_;
+	};
+
+	//bool is_concatenation_;
+	int state_count_;
+
+	// 保存动态分配的数据结构，方便释放
+	std::list<State*> state_list_;
+
+	//开始状态和终止状态
+	State* start_state_;
+	State* end_state_;
+
+	State * AllocState();
+
+	// 释放动态分配的State内存
+	void FreeStates();
+};
+
+#endif
